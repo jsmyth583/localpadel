79354abe3b3bb3d26129bec1b887eb30d4e8de17
jsmyth583 <jsmyth583@gmail.com>
2026-02-10 10:12:16 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+
 import Home from "./pages/Home.jsx";
 import Signup from "./pages/Signup.jsx";
 import Profile from "./pages/Profile.jsx";
